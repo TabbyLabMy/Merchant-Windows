8b9de813bc920b21f9ad39e9d7b41b060cd6b8de
lkching7 <134677998+TabbyPOS@users.noreply.github.com>
2023-06-15 15:52:01 +0800

    Upload

--- TabbyPOSMerchant/Balance.vb
+++ TabbyPOSMerchant/Balance.vb
@@ -11,8 +11,5 @@
     End Sub
 
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs)
-        Dim wsTabby As New wsTabby.Service1
-        wsTabby.RegisterNewAccount("lkching7@gmail.com", "abc@123")
-    End Sub
+   
 End Class
